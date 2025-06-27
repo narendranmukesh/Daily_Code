e05521a93f517fda979534b9672e0f0b7b24311a
Narendran Mukesh <132182325+narendranmukesh@users.noreply.github.com>
2025-06-27 12:25:33 +0530

    Update DeleteMid.java

--- DeleteMid.java
+++ DeleteMid.java
@@ -1,39 +1,40 @@
-// package Stacks;
-// import java.util.Stack;
+package Stacks;
+import java.util.Stack;
 
-// public class DeleteMid {
-//     public static void deleteMid(Stack<Integer> st){
-//         Stack<Integer> tempSt=new Stack<>();
-//         int mid = st.size() / 2;
-//         int count=0;
+public class DeleteMid {
+    public static void deleteMid(Stack<Integer> st){
+        Stack<Integer> tempSt=new Stack<>();
+        int mid = st.size() / 2;
+        int count=0;
 
-//         while(count <mid){
-//             int c=st.pop();
-//             tempSt.push(c);
-//             count++;
-//         }
-//         st.pop();
+        while(count <mid){
+            int c=st.pop();
+            tempSt.push(c);
+            count++;
+        }
+        st.pop();
 
-//         while(!tempSt.isEmpty()){
-//             st.push(tempSt.pop());
-//         }
+        while(!tempSt.isEmpty()){
+            st.push(tempSt.pop());
+        }
 
-//     }
-//     public static void main(String[] args) {
-//         Stack<Integer> st = new Stack<>();
-//         st.push(1);
-//         st.push(2);
-//         st.push(3);
-//         st.push(4);
-//         st.push(5);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
 
-//         System.out.println("Original Stack: " + st);
-//         deleteMid(st);
-//         System.out.println("Stack after deleting middle element: " + st);   
+        System.out.println("Original Stack: " + st);
+        deleteMid(st);
+        System.out.println("Stack after deleting middle element: " + st);   
         
-//     }
+    }
     
-// }
+}
+                //USING ARRAYLIST
 
 package Stacks;
 
